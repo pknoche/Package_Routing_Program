e08df38fefa35bba09e2b41f4ebefa9eaee76a1f
Philipp Knoche <knoche.philipp@gmail.com>
2023-06-11 17:08:01 -0500

    Efficiency optimizations

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ import packages
 import routing
 # TODO - add addresses to dictionary instead of list
 
-all_packages = packages.import_packages('data/WGUPS Package File.csv')
+all_packages = packages.PackageCollection()
+all_packages.import_packages('data/WGUPS Package File.csv')
 
-all_addresses = routing.Addresses()
+all_addresses = routing.AddressCollection()
 all_addresses.import_addresses('data/WGUPS Address Table.csv')
 
-all_distances = routing.Distances()
+all_distances = routing.DistanceCollection()
 all_distances.import_distances('data/WGUPS Distance Table.csv')
 
 
-print(all_distances.distance_between(all_addresses, all_addresses.hub_address, '2010 W 500 S'))
+
+
 
 
 
@@ -20,7 +22,15 @@ print(all_distances.distance_between(all_addresses, all_addresses.hub_address, '
 '''
 i = 1
 while i <= 40:
-    print(packages.all_packages.search(i))
+    print(all_packages.all_packages.search(i))
     i += 1
 
+for address in all_addresses.all_addresses:
+    print(address)
+
+for distance_list in all_distances.all_distances:
+    print(distance_list)
+
+print(all_distances.distance_between(all_addresses, (all_addresses.hub_address.street + ' ' + all_addresses.hub_address.zipcode), '2010 W 500 S 84104'))
+print(f'Hub address: {all_addresses.hub_address}')
 '''
